6c5f78798ae695f2d02771abf8e314d30b22b485
Khalid Atayee <khalid.atayee101@gmail.com>
2022-07-28 10:13:35 +0430

    all set now im going to apply localization to my project

---
+++ resources/lang/dr/language.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "dashboard"              =>     "داشبورد",
+    "simcard-management"     =>     "مدیریت سیمکارت ها",
+    "search-staff"           =>     "جستجوی کارمندان",
+    "distribution-simcards"  =>     "توزیع سیمکارت ها",
+    "unit-management"        =>     "مدیریت قطعات",
+    "rank-managemnt"         =>     "مدیریت رتبه",
+    "reports"                =>     "راپورها",
+    "unit-reports"           =>     "راپور قطعات",
+    "user-management"        =>     "مدیریت یوزر ها",
+    "roles"                  =>     "رول ها",
+    "permissions"            =>     "صلاحیت ها",
+];
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ resources/lang/en/language.php
@@ -0,0 +1,27 @@
+<?php
+return [
+    "dashboard"              =>     "Dashboard",
+    "simcard-management"     =>     "Simcards Management",
+    "search-staff"           =>     "Search Staff",
+    "distribution-simcards"  =>     "Simcard Distribution",
+    "unit-management"        =>     "Unit Management",
+    "rank-managemnt"         =>     "Rank Management",
+    "reports"                =>     "Reports",
+    "unit-reports"           =>     "Unit Reports",
+    "user-management"        =>     "User Management",
+    "roles"                  =>     "Roles",
+    "permissions"            =>     "Permissions",
+];
+
+
+
+
+
+
+
+
+
+
+
+
+?>
